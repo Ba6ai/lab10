584f716f4eacee591b17caee91c2846d1979e119
Ba6ai <eliseimartelov292@gmail.com>
2026-05-17 19:46:11 +0500

    Initial commit

---
+++ Main/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Compiler;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string FilePath = "C:\\Users\\elise\\OneDrive\\УНИК\\2Курс\\2\\ЯП\\kt4\\lab10\\Main";
+        try
+        {
+            InputOutput.File = new StreamReader(FilePath);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return;
+        }
+
+        Console.WriteLine("Тест ввода-вывода");
+
+        string firstLine = InputOutput.File.ReadLine();
+
+
+    }
+}
